a3591ed91e57daebff59d08be8d2e5ec6f96cf0e
a6h15hek <a6h15hek@outlook.com>
2024-07-19 16:29:42 +0530

    project initialization

---
+++ views/index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import { ThemeProvider, THEME_ID, createTheme } from '@mui/material/styles';
+import {AuthenticationContextProvider} from './AuthenticationContext';
+import {GlobalStatusContextProvider} from './GlobalStatusContext';
+
+
+const materialTheme = createTheme({
+  palette: {
+    mode: 'dark',
+    error: { main: '#e12b38' },
+    warning: { main: '#fcc133' },
+    info: { main: '#3778c2' },
+    primary: { main: '#32064A' },
+    secondary: { main: '#292930' }
+  }
+});
+
+function MainApp() {
+  return (
+    <ThemeProvider theme={{ [THEME_ID]: materialTheme }} >
+      <GlobalStatusContextProvider>
+        <AuthenticationContextProvider>
+          <div>Hello World</div>
+        </AuthenticationContextProvider>
+      </GlobalStatusContextProvider>
+    </ThemeProvider>
+  );
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <MainApp />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
